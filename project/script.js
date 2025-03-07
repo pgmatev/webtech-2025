e0ba80f6d9e0c2e16b99a4d28f3a01572345c0b3
Petar-Gabriel Matev <pmatev@asteasolutions.com>
2025-03-07 13:01:02 +0200

    added html pages

--- project/script.js
+++ project/script.js
@@ -1,2 +1,16 @@
-// document.getElementById, getElementByClass, querySelector
-// createElement, appendChild, removeChild ...
+const button = document.getElementById("add-expense-btn");
+
+button.addEventListener("click", () => {
+  const inputName = document.getElementById("expense-name");
+  const inputAmount = document.getElementById("expense-amount");
+
+  const list = document.getElementById("expenses-list");
+  const newItem = document.createElement("li");
+
+  newItem.classList.add("list-item");
+  newItem.textContent = `${inputName.value}: ${inputAmount.value} lv`;
+  list.appendChild(newItem);
+
+  inputName.value = "";
+  inputAmount.value = "";
+});
